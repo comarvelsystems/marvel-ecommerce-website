223cf04e2460578cdfc6a16a1a3daa898e74207f
comarvelsystems <tkahmedkamal@marvel.com.sa>
2024-10-14 15:06:56 +0300

    Edit placeholder navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,28 @@
-import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
+import { Heart, MoveLeft, ShoppingCart } from "lucide-react";
 import CartSheet from "../CartSheet";
 
 const Navbar = () => {
   return (
     <nav className='h-20 bg-muted'>
       <div className='s-container'>
-        <div className='flex h-20 items-center justify-end'>
-          <CartSheet>
-            <div className='flex size-14 cursor-pointer items-center justify-center rounded-full border border-border bg-white text-destructive'>
-              <ShoppingCart />
-            </div>
-          </CartSheet>
+        <div className='flex h-20 items-center justify-between gap-4'>
+          <Link href='/'>
+            <MoveLeft size={28} />
+          </Link>
+          <div className='flex items-center gap-2'>
+            <Link
+              href='/wishlist'
+              className='flex size-14 cursor-pointer items-center justify-center rounded-full border border-border bg-white'
+            >
+              <Heart />
+            </Link>
+            <CartSheet>
+              <div className='flex size-14 cursor-pointer items-center justify-center rounded-full border border-border bg-white text-destructive'>
+                <ShoppingCart />
+              </div>
+            </CartSheet>
+          </div>
         </div>
       </div>
     </nav>
